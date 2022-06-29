000743c3152a457d222694e738adfd4a969695b6
uptocorrupt <1113051093@qq.com>
2022-06-29 13:44:52 +0800

    增加底部版权信息显示

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
     <router-view />
+    <div
+      style="position:fixed;text-align:center;bottom:0;margin:0 auto;width:100%;color: #5c6b77"
+    >
+      <a
+        target="_blank"
+        style="color: #5c6b77"
+        href="https://beian.miit.gov.cn/"
+        >桂ICP备2022005935号</a
+      >
+      &nbsp; ©2021-2022 秋叶原の黑猫
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
